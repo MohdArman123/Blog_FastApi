3b454afcd0bf25b3c02aa8c39a768c5f84af3de6
Arman <armansheakh987@gmail.com>
2025-03-10 17:35:14 +0530

    add home route

--- blog/main.py
+++ blog/main.py
@@ -7,6 +7,10 @@ app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
+@app.get("/")
+def home():
+    return {"message": "Welcome to FastAPI Blog API!"}
+
 app.include_router(authentication.router)
 app.include_router(blog.router)
 app.include_router(user.router)
