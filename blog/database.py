57ad0f6ba65b3b331a58a0aae1d2ca371746cb56
Arman <armansheakh987@gmail.com>
2025-03-13 12:52:54 +0530

    add dependency dotenv

--- blog/database.py
+++ blog/database.py
@@ -2,9 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()  # Load .env variables
+
 # SQLALCHEMY_DATABASE_URL =  "sqlite:///./blog.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost/blogdb"
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:1234@localhost/blogdb")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread": False})
